e9aea3a7e36e51bbfa4b64fc142611bfda10a196
M Tayyab Saeed <tayyab.saeed05@gmail.com>
2017-11-10 01:54:17 +0100

    The todo has now object having some properties being pushed to it
    The object has two properties: one is adding text to it, the toggles
    with a toggleCompleted method for the position at which text changes.

--- js/todosOperations.js
+++ js/todosOperations.js
@@ -1,28 +1,44 @@
 let todoList = {
     //declaring todos array as a property
-    todos: ['item1', 'item2', 'item3'],
+    todos: [],
 
     displayTodos: function () {
         console.log('My Todos: ', this.todos);
     },
 
-    addTodos: function (todoItem) {
-        this.todos.push(todoItem);
+    addTodos: function (todoText) {
+        //passing an oject as an element to the todos arrray
+        this.todos.push(
+            {
+                //this passed object hss two properties
+                todoText: todoText,
+                completed: false
+            }
+        );
         this.displayTodos();
     },
 
-    changeTodo: function (position, todoItem) {
-        this.todos[position] = todoItem;
+    changeTodo: function (position, todoText) {
+        //changing text at a specific index in the object array
+        this.todos[position].todoText = todoText;
         this.displayTodos();
     },
 
     deleteTodo: function (position) {
         this.todos.splice(position, 1);
         this.displayTodos();
+    },
+
+    toggleCompleted: function (position) {
+        let todo = this.todos[position];
+        todo.completed = !todo.completed;
+        this.displayTodos();
     }
+
 };
 
 todoList.displayTodos();
-todoList.addTodos('item4');
-todoList.changeTodo(2, 'plunker');
+todoList.addTodos('some text is here');
+todoList.changeTodo(0, 'plunkerd ddss sdsds');
+todoList.toggleCompleted(0)
 todoList.deleteTodo(0);
